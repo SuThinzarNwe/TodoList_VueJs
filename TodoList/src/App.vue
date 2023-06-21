08a0d5e7748605333b91ee0b8588fcf155a7cdd6
SuThinzarNwe <83086235+SuThinzarNwe@users.noreply.github.com>
2023-06-21 13:33:14 +0630

    Todo List

---
+++ TodoList/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <todo msg="Welcome to Your Vue.js App"> </todo>
+</template>
+
+<script>
+import Todo from "./Todo.vue";
+
+export default {
+  name: "App",
+  components: {
+    Todo,
+  },
+};
+</script>
---
+++ TodoList/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    // Paths to your Vue.js components
+    "./src/**/*.vue",
+    "./public/**/*.html",
+  ],
+  content: [],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
